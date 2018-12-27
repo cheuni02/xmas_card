d895cecd27893065f35698986bf39345e19e4d96
Ivan Cheung <ivan.cheung@bbc.co.uk>
2018-12-27 13:50:39 +0000

    first test xmas card to friend

---
+++ actions.js
@@ -0,0 +1,11 @@
+                function makeMessageAppear(n){
+                  var a = document.getElementById(`message${n}`);
+                  var b = document.getElementById(`button${n}`);
+                  a.style.display='block';
+                  b.style.display='block';
+                }
+                
+                function makeImageAppear(){
+                  document.getElementById(`amy`).style.display = 'block';  
+                  document.getElementById(`button3`).style.display = 'block';
+                }
